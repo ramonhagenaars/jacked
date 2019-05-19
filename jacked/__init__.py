c1b77153c188e3e026fa292148b8cedd601d1f57
Ramon <r.amonhagenaars@gmail.com>
2019-05-19 17:47:10 +0200

    Added discover function to the jacked package.

--- jacked/__init__.py
+++ jacked/__init__.py
@@ -1,7 +1,9 @@
 import jacked._inject
 import jacked._injectable
+import jacked._discover
 
 
 inject = jacked._inject.inject
 injectable = jacked._injectable.injectable
 Injectable = jacked._injectable.Injectable
+discover = jacked._discover.discover
