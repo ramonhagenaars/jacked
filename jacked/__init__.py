6b6728a4bcc5bda2d775d96d89ac5127d01a25f5
Ramon <r.amonhagenaars@gmail.com>
2019-05-23 16:25:40 +0200

    Made Exceptions importable.

--- jacked/__init__.py
+++ jacked/__init__.py
@@ -1,9 +1,18 @@
 import jacked._inject
 import jacked._injectable
 import jacked._discover
+import jacked._exceptions
 
 
+# Classes:
+Injectable = jacked._injectable.Injectable
+
+# Functions:
 inject = jacked._inject.inject
 injectable = jacked._injectable.injectable
-Injectable = jacked._injectable.Injectable
 discover = jacked._discover.discover
+
+# Exceptions:
+JackedError = jacked._exceptions.JackedError
+InvalidUsageError = jacked._exceptions.InvalidUsageError
+InjectionError = jacked._exceptions.InjectionError
