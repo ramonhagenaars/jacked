6e8b0bbab4b4455650ee467f1398a938ef184fa1
Ramon <p8u7wAPC5Pg9HYkkCkzA>
2019-06-07 14:32:32 +0200

    Provided some types.

--- jacked/__init__.py
+++ jacked/__init__.py
@@ -1,11 +1,18 @@
 import jacked._inject
 import jacked._injectable
+import jacked._container
+import jacked._types
 import jacked._discover
 import jacked._exceptions
 
 
-# Classes:
+# Types:
 Injectable = jacked._injectable.Injectable
+Container = jacked._container.Container
+T = jacked._types.T
+Module = jacked._types.Module
+NoneType = jacked._types.NoneType
+AttrDict = jacked._types.AttrDict
 
 # Functions:
 inject = jacked._inject.inject
